63cf8fffda053a189f99796d5632fb2287676e81
Robert Balderas <robert@rbdevelop.com>
2015-05-13 00:51:21 -0700

    adding style to home page. this version has clearly named routes and pages that make sense

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root 'home#index'
+
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -70,4 +75,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
