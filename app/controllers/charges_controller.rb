a236c3ad0adfb621044c5b424998e4727354c3c4
leofrozenyogurt <rebellabs@gmail.com>
2015-05-19 23:33:36 -0400

    remove turbolinks

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,7 +5,7 @@ class ChargesController < ApplicationController
     (@transaction = StripeWrapper::Card.new).create_or_update_customer_by_transaction_token(params['credit_card_token_id'], "#{@current_parent.first_name} #{@current_parent.last_name}")
     .set_transaction(1000, currency="USD", "#{@current_parent.first_name} #{@current_parent.last_name}", "Charge")
     .perform_transaction!
-    Charge.create(parent_id: current_parent.id)
+    Charge.create(parent_id: current_parent.id, amount: 10)
     respond_to do |format|
       format.html {
         flash[:alert]=@errors
