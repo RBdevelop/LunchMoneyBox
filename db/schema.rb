55ccb775df90ed0ea5db636d6857f98834ad2bb9
Gabe Rubio <gtrubio192@gmail.com>
2015-05-25 23:28:09 -0500

    Touching up 404 page

--- db/schema.rb
+++ db/schema.rb
@@ -79,11 +79,6 @@ ActiveRecord::Schema.define(version: 20150520033119) do
   add_index "parents", ["email"], name: "index_parents_on_email", unique: true, using: :btree
   add_index "parents", ["reset_password_token"], name: "index_parents_on_reset_password_token", unique: true, using: :btree
 
-  create_table "payments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
