f1327039b4ea695b61b471c360502b03094fbed0
Robert Balderas <robert@rbdevelop.com>
2015-05-27 00:49:39 -0700

    setting up mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,11 +7,6 @@ Rails.application.configure do
           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
         }
       }
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -19,10 +14,25 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
+
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port:3000}
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'lunchmoneybox.io',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'support@lunchmoneybox.io',
+    password: '2015SAcatering',
+    :openssl_verify_mode  => 'none'
+    }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -46,4 +56,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
 end
